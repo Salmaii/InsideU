d2f9458d3fe59f29e5416547b155dcaeb1594355
Gabriel Salmai <bile.slamai@gmail.com>
2023-06-03 11:19:48 -0300

    Improve Splash Screen Performance #31

--- mobile/lib/src/main.dart
+++ mobile/lib/src/main.dart
@@ -1,14 +1,68 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_modular/flutter_modular.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+// import 'app_module.dart';
+// import 'app_widget.dart';
+
+// import 'package:firebase_core/firebase_core.dart';
+// import '../firebase_options.dart';
+
+// void main() async {
+//   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+
+//   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+//   runApp(
+//     ModularApp(module: AppModule(), child: const AppWidget()),
+//   );
+// }
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app_module.dart';
 import 'app_widget.dart';
 
-void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(
-    ModularApp(module: AppModule(), child: const AppWidget()),
+import '../firebase_options.dart';
+
+Future<void> initializeApp() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Aqui você pode executar outras tarefas assíncronas que precisa realizar durante a splash screen
+}
+
+void main() {
+  runApp(const SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FutureBuilder(
+          future: initializeApp(),
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ModularApp(module: AppModule(), child: const AppWidget());
+            } else {
+              return const Center(
+                  child: CircularProgressIndicator(
+                color: Colors.white,
+              ));
+            }
+          },
+        ),
+      ),
+    );
+  }
 }
