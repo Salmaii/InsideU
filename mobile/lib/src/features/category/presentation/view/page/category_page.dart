dd254df2257410e0506c565e83c609d695fe2a1e
Gabriel Salmai <bile.slamai@gmail.com>
2023-06-03 11:33:13 -0300

    Add cateogry page sketch #38

--- mobile/lib/src/features/category/presentation/view/page/category_page.dart
+++ mobile/lib/src/features/category/presentation/view/page/category_page.dart
@@ -1,42 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:localization/localization.dart';
 import 'package:InLaw/src/theme.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-class CategoryPage extends StatefulWidget {
-  @override
-  _CategoryPageState createState() => _CategoryPageState();
-}
+class CategoryPage extends StatelessWidget {
+  final String titulo;
 
-class _CategoryPageState extends State<CategoryPage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  CategoryPage({Key? key, required this.titulo}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          backgroundColor: Color(0xFF011C2E),
-          centerTitle: false,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                _scaffoldKey.currentState?.openEndDrawer();
-              },
-            ),
-          ],
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Text('username'),
-          ),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titulo),
       ),
+      // Restante do conteúdo da página
     );
   }
 }
