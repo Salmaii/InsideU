4c65a6716bd0a35e031737e9c29c44e518159241
Gabriel Salmai <bile.slamai@gmail.com>
2023-06-13 15:30:02 -0300

    Add different onboard animation #64

--- mobile/lib/src/features/onboarding/presentation/view/page/onboarding_page.dart
+++ mobile/lib/src/features/onboarding/presentation/view/page/onboarding_page.dart
@@ -1,4 +1,3 @@
-import 'package:InLaw/src/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:localization/localization.dart';
@@ -20,17 +19,17 @@ class _OnboardingPageState extends State<OnboardingPage> {
     {
       'text': 'Onboard_text1'.i18n(),
       'image': 'assets/images/onboarding_page_1.png',
-      'animation': 'lib/assets/images/legal_statement.json',
+      'animation': 'lib/assets/animations/legal_statement.json',
     },
     {
       'text': 'Onboard_text2'.i18n(),
       'image': 'assets/images/onboarding_page_2.png',
-      'animation': 'lib/assets/images/legal_statement.json',
+      'animation': 'lib/assets/animations/law_weight.json',
     },
     {
       'text': 'Onboard_text3'.i18n(),
       'image': 'assets/images/onboarding_page_3.png',
-      'animation': 'lib/assets/images/legal_statement.json',
+      'animation': 'lib/assets/animations/deal.json',
     },
   ];
 
@@ -46,75 +45,72 @@ class _OnboardingPageState extends State<OnboardingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: getTheme(),
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: PageView.builder(
-                  controller: _pageController,
-                  itemCount: onboardingData.length,
-                  onPageChanged: (int page) {
-                    setState(() {
-                      _currentPage = page;
-                    });
-                  },
-                  itemBuilder: (context, index) {
-                    return buildOnboardingPage(index);
-                  },
-                ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: onboardingData.length,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return buildOnboardingPage(index);
+                },
               ),
-              SizedBox(height: 100.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  if (_currentPage != 0)
-                    ElevatedButton(
-                      onPressed: () {
-                        _pageController.previousPage(
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.ease,
-                        );
-                      },
-                      child: Text(
-                        'Back'.i18n(),
-                        style: TextStyle(fontSize: 18.0),
-                      ),
+            ),
+            SizedBox(height: 100.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                if (_currentPage != 0)
+                  ElevatedButton(
+                    onPressed: () {
+                      _pageController.previousPage(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                    },
+                    child: Text(
+                      'Back'.i18n(),
+                      style: TextStyle(fontSize: 18.0),
                     ),
-                  if (_currentPage != onboardingData.length - 1)
-                    ElevatedButton(
-                      onPressed: () {
-                        _pageController.nextPage(
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.ease,
-                        );
-                      },
-                      child: Text(
-                        'Next'.i18n(),
-                        style: TextStyle(fontSize: 18.0),
-                      ),
+                  ),
+                if (_currentPage != onboardingData.length - 1)
+                  ElevatedButton(
+                    onPressed: () {
+                      _pageController.nextPage(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                    },
+                    child: Text(
+                      'Next'.i18n(),
+                      style: TextStyle(fontSize: 18.0),
                     ),
-                  if (_currentPage == onboardingData.length - 1)
-                    ElevatedButton(
-                      onPressed: () {
-                        Modular.to.pushReplacementNamed('/login/');
-                      },
-                      child: Text(
-                        'Login'.i18n(),
-                        style: TextStyle(fontSize: 18.0),
-                      ),
+                  ),
+                if (_currentPage == onboardingData.length - 1)
+                  ElevatedButton(
+                    onPressed: () {
+                      Modular.to.pushReplacementNamed('/login/');
+                    },
+                    child: Text(
+                      'Login'.i18n(),
+                      style: TextStyle(fontSize: 18.0),
                     ),
-                ],
-              ),
-              const SizedBox(height: 50.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: buildPageIndicator(),
-              ),
-            ],
-          ),
+                  ),
+              ],
+            ),
+            const SizedBox(height: 50.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: buildPageIndicator(),
+            ),
+          ],
         ),
       ),
     );
