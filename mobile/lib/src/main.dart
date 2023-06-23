2b926d08c685d255d4808bff2b9156371b2b0558
Gabriel Salmai <bile.slamai@gmail.com>
2023-06-23 20:38:17 -0300

    Remove degub banner #75

--- mobile/lib/src/main.dart
+++ mobile/lib/src/main.dart
@@ -26,6 +26,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: FutureBuilder(
           future: initializeApp(),
