421ec02011f875317aafc39174daca5371fe3381
Gabriel Salmai <bile.slamai@gmail.com>
2023-06-23 20:51:50 -0300

    Remove unnused

--- mobile/lib/src/features/onboarding/presentation/view/page/onboarding_page.dart
+++ mobile/lib/src/features/onboarding/presentation/view/page/onboarding_page.dart
@@ -40,7 +40,6 @@ class _OnboardingPageState extends State<OnboardingPage> {
   }
 
   void initialization() async {
-    // WidgetsBinding.instance?.addPostFrameCallback((_) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       FlutterNativeSplash.remove();
     });
