b5fa7924c6e1eaaf79b0502b9a8619e1568cbe37
Gabriel Salmai <bile.slamai@gmail.com>
2023-06-23 04:46:56 -0300

    Remove unnused

--- mobile/lib/src/features/category/store/category_model.dart
+++
@@ -1,7 +0,0 @@
-class CategoryModel {
-  String? key;
-  String nome;
-  int prioridade;
-
-  CategoryModel({required this.nome, required this.prioridade});
-}
--- mobile/lib/src/services/db_service.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
-// class DBService {
-//   final _db = FirebaseFirestore.instance;
-// }
